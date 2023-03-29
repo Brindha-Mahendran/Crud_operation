bfcbc094949d0099f28633bc6a4f42b5f51d294e
Brindha-Mahendran <115245427+Brindha-Mahendran@users.noreply.github.com>
2023-03-29 14:35:43 +0530

    Create User.java

---
+++ Crud_Operation/src/main/java/com/kce/model/User.java
@@ -0,0 +1,65 @@
+package com.kce.model;
+
+public class User {
+	protected int id;
+	protected String first_name;
+	protected String last_name;
+	protected String email;
+	protected String password;
+	protected String phone_num;
+	
+	public User(String first_name, String last_name, String email, String password, String phone_num) {
+		super();
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.password = password;
+		this.phone_num = phone_num;
+	}
+	public User(int id, String first_name, String last_name, String email, String password, String phone_num) {
+		super();
+		this.id = id;
+		this.first_name = first_name;
+		this.last_name = last_name;
+		this.email = email;
+		this.password = password;
+		this.phone_num = phone_num;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getFirst_name() {
+		return first_name;
+	}
+	public void setFirst_name(String first_name) {
+		this.first_name = first_name;
+	}
+	public String getLast_name() {
+		return last_name;
+	}
+	public void setLast_name(String last_name) {
+		this.last_name = last_name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getPhone_num() {
+		return phone_num;
+	}
+	public void setPhone_num(String phone_num) {
+		this.phone_num = phone_num;
+	}
+	
+}
